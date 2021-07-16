ce69afcae6a5d2fbbc2c8b9e690cf81c6ab719f9
Shinji Iida <natsure@siida-macbook.local>
2021-07-16 18:22:44 +0900

    nve shell was added and minor modifications

--- 03_nve_prod.sh
+++ 03_nve_prod.sh
@@ -9,13 +9,13 @@ EOS
 id=$1
 
 echo "NPT runs are running..."
-cp templates/smd.mdp smd_${id}.mdp
-gmx grompp -f smd_${id}.mdp  \
+cp templates/nve_prod.mdp nve_prod_${id}.mdp
+gmx grompp -f nve_prod_${id}.mdp  \
            -c npt_eq_${id}.gro    \
            -t npt_eq_${id}.cpt    \
            -p topol.top           \
-           -o smd_${id}.tpr
+           -o nve_prod_${id}.tpr
 # - Starting coordinates can be read from trajectory with -t
 #   - Only if this information is absent will the coordinates in the -c file be used.    
 
-gmx mdrun -deffnm smd_${id}
+gmx mdrun -deffnm nve_prod_${id}
--- 03_ramd.sh
+++ 03_ramd.sh
@@ -11,10 +11,10 @@ id=$1
 echo "NPT runs are running..."
 cat templates/ramd.mdp | sed -e "s!#{SEED}!${RANDOM}!g" > ramd_${id}.mdp
 gmx grompp -f ramd_${id}.mdp  \
-            -c npt_eq_${id}.gro    \
-            -t npt_eq_${id}.cpt    \
-            -p topol.top           \
-            -o ramd_${id}.tpr
+           -c npt_eq_${id}.gro    \
+           -t npt_eq_${id}.cpt    \
+           -p topol.top           \
+           -o ramd_${id}.tpr
 # - Starting coordinates can be read from trajectory with -t
 #   - Only if this information is absent will the coordinates in the -c file be used.    
 
