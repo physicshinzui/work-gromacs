06e341aa48d227396cdb5ba65da5f350cbdc0863
siida <physicshinzui@gmail.com>
2021-08-23 14:28:56 +0900

    Updated: 03_searchforFmax.sh       

--- 03_searchforFmax.sh
+++ 03_searchforFmax.sh
@@ -11,25 +11,25 @@ while [ $((f_end-f_start)) -gt 4 ]
 do
     f_mid=$(( ($f_start+$f_end)/2 ))
     echo "f_start=${f_start}, f_end=${f_end}, f_mid=${f_mid}" >> ${logfile}
-    cat templates/smd_Fmax.mdp | sed -e "s!#{FORCE_CONST}!${f_mid}!g"| grep "pull_coord1_k" > pull_k${f_mid}.mdp
+    cat templates/smd_Fmax.mdp | sed -e "s!#{FORCE_CONST}!${f_mid}!g" > pull_k${f_mid}.mdp
 
-    # gmx grompp -f pull_${f_mid}.mdp -c npt_eq_${id}.gro -o pull -p topol.top -n index.ndx -maxwarn 1
-    # mdrun -v -deffnm pull -px pull_${f_mid}.xvg -pf pull_${f_mid}.xvg -ntmpi 1 -ntomp 20 >& pull_${f_mid}.job
+    echo "gmx grompp -f pull_${f_mid}.mdp -c npt_eq_${id}.gro -o pull -p topol.top -n index.ndx -maxwarn 1"
+    echo "mdrun -v -deffnm pull -px pull_${f_mid}.xvg -pf pull_${f_mid}.xvg -ntmpi 1 -ntomp 20 >& pull_${f_mid}.job"
 
-    outfile=pull_${f_mid}.gro 
+    outfile=pull_${f_mid}.gro 
 
-    if test -f "$outfile" ; then
-        echo ".gro file was generated when pull force = ${f_mid}" >> ${logfile}
-        f_start=$f_mid
-
-    else
-        echo "NO .gro file was generated when pull force = ${f_mid}" >> ${logfile}
-        f_end=$f_mid
-    fi
-
+    if test -f "$outfile" ; then
+        echo ".gro file was generated when pull force = ${f_mid}" >> ${logfile}
+        f_start=$f_mid
+    
+    else
+        echo "NO .gro file was generated when pull force = ${f_mid}" >> ${logfile}
+        f_end=$f_mid
+    fi
+    
     # if f_end - f_start == 4, output the f_start value (aka Fmax) to summary file
-    if [ $((f_end-f_start)) -le 4 ]; then
-        echo "autocheck-1, ${f_start}" >> ${summary_file}
-    fi
+    if [ $((f_end-f_start)) -le 4 ]; then
+        echo "autocheck-1, ${f_start}" >> ${summary_file}
+    fi
 
 done
