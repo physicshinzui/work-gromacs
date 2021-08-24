83e0a1aca8cd5cb80a6e864760f05408c84b6718
siida <physicshinzui@gmail.com>
2021-08-24 13:55:49 +0900

    update: 01_prepMD.sh

--- 01_prepMD.sh
+++ 01_prepMD.sh
@@ -10,25 +10,26 @@ EOS
 
 inputPDBName=$1 
 proteinName=`basename ${inputPDBName%.*}`
+GMX=gmx
 
-gmx pdb2gmx -f ${inputPDBName} -o ${proteinName}_processed.gro -water tip3p
+${GMX} pdb2gmx -f ${inputPDBName} -o ${proteinName}_processed.gro -water tip3p
 
-gmx editconf -f ${proteinName}_processed.gro \
+${GMX} editconf -f ${proteinName}_processed.gro \
               -o ${proteinName}_newbox.gro    \
               -d 1.0                          \
               -bt dodecahedron #triclinic 
 
-gmx solvate -cp ${proteinName}_newbox.gro \
+${GMX} solvate -cp ${proteinName}_newbox.gro \
              -cs spc216.gro                \
              -o ${proteinName}_solv.gro    \
              -p topol.top
 
-gmx grompp -f templates/ions.mdp \
+${GMX} grompp -f templates/ions.mdp \
             -c ${proteinName}_solv.gro \
             -p topol.top \
             -o ions.tpr
 
-echo "SOL" | gmx genion \
+echo "SOL" | ${GMX} genion \
     -s ions.tpr \
     -o ${proteinName}_solv_ions.gro \
     -p topol.top \
@@ -36,16 +37,16 @@ echo "SOL" | gmx genion \
     -conc 0.1 -neutral 
 
 echo "Energy minimisation 1 ..."
-gmx grompp -f templates/em1.mdp \
+${GMX} grompp -f templates/em1.mdp \
             -c ${proteinName}_solv_ions.gro \
             -r ${proteinName}_solv_ions.gro \
             -p topol.top \
             -o em1.tpr 
-gmx mdrun -deffnm em1 
+${GMX} mdrun -deffnm em1 
 
 echo "Energy minimisation 2 ..."
-gmx grompp -f templates/em2.mdp \
+${GMX} grompp -f templates/em2.mdp \
             -c em1.gro \
             -p topol.top \
             -o em2.tpr 
-gmx mdrun -deffnm em2
+${GMX} mdrun -deffnm em2
