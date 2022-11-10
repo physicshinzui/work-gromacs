a3f15f168851097bafd5e215cbe2ac0acdd8a0a4
siida <physicshinzui@gmail.com>
2022-11-10 18:12:35 +0900

    modified

--- test/test_nve.sh
+++ test/test_nve.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
+export GMX=gmx
 ## NVE in Vaccume 
-echo 1 | ./00_vac_nvt.sh sample_pdb/decaAla.pdb 100000 nve >& log1
-echo "Total-Energy" |  gmx energy -f nve_vac_1.edr -o totene.xvg >& log2
-cat log2 | grep -2 "Total Energy"
+echo 1 | ./00_vaccum.sh sample_pdb/decaAla.pdb 100000 nve >& /dev/null
+echo "Total-Energy" |  $GMX energy -f nve_vac_1.edr -o totene.xvg >& log
+cat log | grep -2 "Total Energy"
