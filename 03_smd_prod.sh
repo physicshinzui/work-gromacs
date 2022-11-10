6077fed16add81454e1b4d0824509f2031a01b4d
siida <physicshinzui@gmail.com>
2022-11-10 17:01:42 +0900

    -po option for grompp was added

--- 03_smd_prod.sh
+++ 03_smd_prod.sh
@@ -11,13 +11,14 @@ id=$1
 echo "SMD runs are running..."
 
 # Pulling
-cp templates/SMD_constVelo.mdp smd_${id}.mdp
-gmx grompp -f smd_${id}.mdp  \
-           -c npt_eq_${id}.gro    \
-           -t npt_eq_${id}.cpt    \
-           -r npt_eq_${id}.gro    \
-           -p topol.top           \
-           -n index.ndx           \
+cp templates/cv_smd.mdp smd_${id}.mdp
+gmx grompp -f smd_${id}.mdp \
+           -c npt_eq_${id}.gro \
+           -t npt_eq_${id}.cpt \
+           -r npt_eq_${id}.gro \
+           -p topol.top \
+           -po mdout_cv_smd.mdp \
+           -n index.ndx  \
            -o smd_${id}.tpr
 # - Starting coordinates can be read from trajectory with -t
 #   - Only if this information is absent will the coordinates in the -c file be used.    
@@ -25,14 +26,14 @@ gmx grompp -f smd_${id}.mdp  \
 gmx mdrun -deffnm smd_${id}
 
 # Reverse pulling
-cp templates/SMD_constVelo_inverse.mdp smd_${id}.mdp
-gmx grompp -f smd_${id}.mdp  \
-           -c smd_${id}.gro    \
-           -t smd_${id}.cpt    \
-           -r smd_${id}.gro    \
-           -p topol.top           \
-           -n index.ndx           \
-           -o smd_rev${id}.tpr
+cp templates/cv_smd_inv.mdp smd_${id}.mdp
+gmx grompp -f smd_${id}.mdp \
+           -c smd_${id}.gro \
+           -t smd_${id}.cpt \
+           -r smd_${id}.gro \
+           -p topol.top \
+           -po mdout_cv_smd_inv.mdp \
+           -n index.ndx \
+           -o smd_inv_${id}.tpr
 
-gmx mdrun -deffnm smd_rev${id}
-
+gmx mdrun -deffnm smd_inv_${id}
