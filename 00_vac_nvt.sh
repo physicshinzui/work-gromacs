eaec43083f6a285e55318afd787301c48e0fb000
siida <physicshinzui@gmail.com>
2022-11-10 17:08:39 +0900

    mdp and a script for vaccume simulation were added

---
+++ 00_vac_nvt.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eu
+
+cat << EOS
+Author: Shinji Iida
+This script automates a system preparation for Gromacs.
+    Usage:
+        bash ${0} [PDB file]
+EOS
+
+inputPDBName=$1
+proteinName=`basename ${inputPDBName%.*}`
+GMX=gmx
+
+${GMX} pdb2gmx -f ${inputPDBName} -o ${proteinName}_processed.gro -water tip3p
+
+${GMX} editconf -f ${proteinName}_processed.gro \
+              -o ${proteinName}_newbox.gro \
+              -box 1000.0 1000.0 1000.0 \
+              -bt cubic
+
+echo "Energy minimisation 1 ..."
+${GMX} grompp -f templates/em_vac.mdp \
+            -c ${proteinName}_newbox.gro \
+            -r ${proteinName}_newbox.gro \
+            -p topol.top \
+            -po mdout_em.mdp \
+            -o em.tpr
+${GMX} mdrun -deffnm em
+
+echo "NVT equilibration runs are running..."
+id=1
+cat templates/nvt_vac.mdp | sed -e "s!#{RAND}!${RANDOM}!g" > nvt_vac_${id}.mdp
+${GMX} grompp -f nvt_vac_${id}.mdp \
+              -c em.gro \
+              -r em.gro \
+              -p topol.top \
+              -po mdout_nvt_vac.mdp \
+              -o nvt_vac_${id}.tpr
+${GMX} mdrun -deffnm nvt_vac_${id}
