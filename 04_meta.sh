c437f9794523f7f5f487c688a6fb26a06135c38f
siida <physicshinzui@gmail.com>
2022-12-04 22:48:03 +0900

    inputs were changed

--- 04_meta.sh
+++ 04_meta.sh
@@ -6,9 +6,9 @@ PLM=plumed
 id=1
 cp templates/npt_prod.mdp npt_metad_${id}.mdp
 ${GMX} grompp -f npt_metad_${id}.mdp \
-              -c 300K/npt_eq_${id}.gro \
-              -r 300K/npt_eq_${id}.gro \
-              -t 300K/npt_eq_${id}.cpt \
+              -c npt_eq_${id}.gro \
+              -r npt_eq_${id}.gro \
+              -t npt_eq_${id}.cpt \
               -p topol.top \
               -po mdout_npt_metad.mdp \
               -o npt_metad_${id}.tpr
