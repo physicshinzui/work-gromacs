0d3dcea1970922edcf0786559945c6d4c304d739
siida <physicshinzui@gmail.com>
2022-12-08 09:45:01 +0900

    md dir was made

--- md/test/test_nve.sh
+++ md/test/test_nve.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
+set -Ceu
 
-export GMX=gmx
+export GMX=gmx_d
 ## NVE in Vaccume 
-echo 1 | ./00_vaccum.sh sample_pdb/decaAla.pdb 100000 nve >& /dev/null
+echo 1 | ./00_vaccum.sh sample_pdb/decaAla.pdb 100000 nve 
 echo "Total-Energy" |  $GMX energy -f nve_vac_1.edr -o totene.xvg >& log
 cat log | grep -2 "Total Energy"
