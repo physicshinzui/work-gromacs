c3322864ff53454258b86f78759114233cf2c29c
siida <physicshinzui@gmail.com>
2022-12-12 10:34:34 +0900

    init.sh was removed

--- md/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-ls *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
-read -p "Are you really sure?[Enter]"
-read -p "Are you really sure?[Enter]"
-mkdir tmp ; cp -rp *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt tmp
-
-rm -v *.gro *.tpr *# *.edr *.trr *.xtc *.top *.itp *.mdp *.log *.cpt
