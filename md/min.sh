38a8f566d283bf90d3ecbd3d918acf59d05fa57e
z6a419 <physicshinzui>
2023-07-13 18:55:22 +0900

    Add new simple job script

---
+++ md/min.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -eu
+
+inputPDBName=$1 
+BT=$2
+proteinName=`basename ${inputPDBName%.*}`
+GMX=gmx
+MDP_TEMPL='templates/'
+
+${GMX} pdb2gmx -f ${inputPDBName} -o ${proteinName}_processed.gro -water tip3p
+
+${GMX} editconf -f ${proteinName}_processed.gro \
+              -o ${proteinName}_newbox.gro    \
+              -d 1.0                          \
+              -princ \
+              -bt $BT
+
+${GMX} solvate -cp ${proteinName}_newbox.gro \
+             -cs spc216.gro                \
+             -o ${proteinName}_solv.gro    \
+             -p topol.top
+
+${GMX} grompp -f ${MDP_TEMPL}/ions.mdp \
+            -c ${proteinName}_solv.gro \
+            -p topol.top \
+            -po mdout_ion.mdp \
+            -o ions.tpr
+
+echo "SOL" | ${GMX} genion \
+    -s ions.tpr \
+    -o ${proteinName}_solv_ions.gro \
+    -p topol.top \
+    -pname NA -nname CL \
+    -neutral 
+    #-conc 0.1 -neutral 
+
+echo "Energy minimisation 1 ..."
+${GMX} grompp -f ${MDP_TEMPL}/em1.mdp \
+            -c ${proteinName}_solv_ions.gro \
+            -r ${proteinName}_solv_ions.gro \
+            -p topol.top \
+            -po mdout_em1.mdp \
+            -o em1.tpr 
+${GMX} mdrun -deffnm em1 
+
+echo "Energy minimisation 2 ..."
+${GMX} grompp -f ${MDP_TEMPL}/em2.mdp \
+            -c em1.gro \
+            -p topol.top \
+            -po mdout_em2.mdp \
+            -o em2.tpr 
+${GMX} mdrun -deffnm em2
